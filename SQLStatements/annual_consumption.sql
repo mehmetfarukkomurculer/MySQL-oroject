0b4ffdc060c03c78405b08ed09fc78b2aabb1c28
onerayhan <105438463+onerayhan@users.noreply.github.com>
2023-03-28 16:51:23 +0300

    Add files via upload

---
+++ SQLStatements/annual_consumption.sql
@@ -0,0 +1,20 @@
+
+CREATE DATABASE consumption_db;
+USE consumption_db;
+
+CREATE TABLE Countries (
+iso_code VARCHAR(10) NOT NULL,
+country_name VARCHAR(50),
+population BIGINT UNSIGNED,
+median_age REAL,
+PRIMARY KEY (iso_code)
+);
+
+CREATE TABLE Annual_Consumption(
+iso_code VARCHAR(10),
+year_of_data INTEGER NOT NULL,
+renewables_percentage REAL,
+fossil_fuels_percentage REAL,
+PRIMARY KEY (iso_code, year_of_data),
+FOREIGN KEY(iso_code) REFERENCES Countries(iso_code) ON DELETE CASCADE
+);
