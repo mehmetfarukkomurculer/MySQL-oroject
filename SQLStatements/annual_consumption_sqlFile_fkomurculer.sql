5ca995a3c87834d19053c3942d7b534f46675f6f
Mehmet Faruk Kömürcüler <94452557+mfkmrcler@users.noreply.github.com>
2023-03-28 19:04:09 +0300

    Add files via upload

---
+++ SQLStatements/annual_consumption_sqlFile_fkomurculer.sql
@@ -0,0 +1,16 @@
+CREATE TABLE annual_consumption (
+iso_code Varchar(10),
+annual_consumption_year INT,
+total_energy_consumption REAL,
+renewable_consumption REAL,
+fossil_fuel_consumption REAL,
+PRIMARY KEY (iso_code, annual_consumption_year),
+FOREIGN KEY (iso_code) REFERENCES countries(iso_code) ON DELETE CASCADE
+);
+
+
+
+
+
+
+
