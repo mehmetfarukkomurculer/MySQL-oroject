bfeec7b75a17dc38e4ef7524bab084713a501fa6
Mehmet Faruk Kömürcüler <94452557+mfkmrcler@users.noreply.github.com>
2023-04-26 22:01:31 +0300

    Update IN_OPERATOR_fkomurculer_step3.sql

--- SQLStatements/IN_OPERATOR_fkomurculer_step3.sql
+++ SQLStatements/IN_OPERATOR_fkomurculer_step3.sql
@@ -1,14 +1,13 @@
-SELECT countries_name 
-FROM high_gdp_avg 
-WHERE iso_code IN (
-    SELECT iso_code 
-    FROM avg_fossil_fuel_consumption_of_countries 
-    GROUP BY iso_code 
-    HAVING AVG(avg_fossil_fuel_consumption) < (
-        SELECT AVG(avg_fossil_fuel_consumption) 
-        FROM avg_fossil_fuel_consumption_of_countries
+SELECT h.countries_name 
+FROM high_gdp_avg h 
+WHERE h.iso_code IN (
+    SELECT f.iso_code 
+    FROM avg_fossil_fuel_consumption_of_countries f 
+    GROUP BY f.iso_code 
+    HAVING AVG(f.avg_fossil_fuel_consumption) < (
+        SELECT AVG(f2.avg_fossil_fuel_consumption) 
+        FROM avg_fossil_fuel_consumption_of_countries f2
     )
 );
-
 
 
